122e2f9874852d92d51a9e323ddfe8772841af4d
jangoflyte <miguelpcastro95@gmail.com>
2022-09-03 18:01:01 -0600

    started testing

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    //so it can connect to local host
+    baseUrl: "http://localhost:3000",
+  },
+});
